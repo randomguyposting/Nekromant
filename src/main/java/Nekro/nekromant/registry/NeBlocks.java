2a4738acf13615371c4404d413344353598f151b
NouZe <36501230+randomguyposting@users.noreply.github.com>
2022-01-01 00:35:05 +0100

    blocks now drop themselves (yaaaay)

--- src/main/java/Nekro/nekromant/registry/NeBlocks.java
+++ src/main/java/Nekro/nekromant/registry/NeBlocks.java
@@ -2,6 +2,8 @@ package Nekro.nekromant.registry;
 
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -10,9 +12,9 @@ import Nekro.nekromant.Nekromant;
 public class NeBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Nekromant.MODID);
 
-    public static final RegistryObject<Block> PURPLE_CHISELED_DUNGEON_BRICKS = BLOCKS.register("purple_chiseled_dungeon_bricks", () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE)));
-    public static final RegistryObject<Block> PURPLE_DUNGEON_BRICKS = BLOCKS.register("purple_dungeon_bricks", () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE)));
-    public static final RegistryObject<Block> PURPLE_CRACKED_DUNGEON_BRICKS = BLOCKS.register("purple_cracked_dungeon_bricks", () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE)));
-    public static final RegistryObject<Block> PURPLE_MOSSY_DUNGEON_BRICKS = BLOCKS.register("purple_mossy_dungeon_bricks", () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE)));
-    public static final RegistryObject<Block> PURPLE_DUNGEON_PILLAR = BLOCKS.register("purple_dungeon_pillar", () -> new RotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STONE)));
+    public static final RegistryObject<Block> PURPLE_DUNGEON_BRICKS = BLOCKS.register("purple_dungeon_bricks", () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_PINK).strength(0.5f, 6.0f).sound(SoundType.STONE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> PURPLE_CHISELED_DUNGEON_BRICKS = BLOCKS.register("purple_chiseled_dungeon_bricks", () -> new Block(BlockBehaviour.Properties.copy(NeBlocks.PURPLE_DUNGEON_BRICKS.get())));
+    public static final RegistryObject<Block> PURPLE_CRACKED_DUNGEON_BRICKS = BLOCKS.register("purple_cracked_dungeon_bricks", () -> new Block(BlockBehaviour.Properties.copy(NeBlocks.PURPLE_DUNGEON_BRICKS.get())));
+    public static final RegistryObject<Block> PURPLE_MOSSY_DUNGEON_BRICKS = BLOCKS.register("purple_mossy_dungeon_bricks", () -> new Block(BlockBehaviour.Properties.copy(NeBlocks.PURPLE_DUNGEON_BRICKS.get())));
+    public static final RegistryObject<Block> PURPLE_DUNGEON_PILLAR = BLOCKS.register("purple_dungeon_pillar", () -> new RotatedPillarBlock(BlockBehaviour.Properties.copy(NeBlocks.PURPLE_DUNGEON_BRICKS.get())));
 }
