16cae36ab372813902aba515e45a3522c7292b9e
NouZe <36501230+randomguyposting@users.noreply.github.com>
2021-12-31 17:28:07 +0100

    add new blocks

--- src/main/java/Nekro/nekromant/registry/NeBlocks.java
+++ src/main/java/Nekro/nekromant/registry/NeBlocks.java
@@ -11,4 +11,8 @@ public class NeBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Nekromant.MODID);
 
     public static final RegistryObject<Block> PURPLE_CHISELED_DUNGEON_BRICKS = BLOCKS.register("purple_chiseled_dungeon_bricks", () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE)));
+    public static final RegistryObject<Block> PURPLE_DUNGEON_BRICKS = BLOCKS.register("purple_dungeon_bricks", () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE)));
+    public static final RegistryObject<Block> PURPLE_CRACKED_DUNGEON_BRICKS = BLOCKS.register("purple_cracked_dungeon_bricks", () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE)));
+    public static final RegistryObject<Block> PURPLE_MOSSY_DUNGEON_BRICKS = BLOCKS.register("purple_mossy_dungeon_bricks", () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE)));
+    public static final RegistryObject<Block> PURPLE_DUNGEON_PILLAR = BLOCKS.register("purple_dungeon_pillar", () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE)));
 }
