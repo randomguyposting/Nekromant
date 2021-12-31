3fc44f4a6b4bf0fe1832466ac1dcea452ad47159
NouZe <36501230+randomguyposting@users.noreply.github.com>
2021-12-31 17:35:26 +0100

    move blocks to new creative tab

--- src/main/java/Nekro/nekromant/registry/NeItems.java
+++ src/main/java/Nekro/nekromant/registry/NeItems.java
@@ -11,13 +11,11 @@ public class NeItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Nekromant.MODID);
 
 
-    public static final RegistryObject<BlockItem> PURPLE_CHISELED_DUNGEON_BRICKS = ITEMS.register("purple_chiseled_dungeon_bricks", () -> new BlockItem(NeBlocks.PURPLE_CHISELED_DUNGEON_BRICKS.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
-    public static final RegistryObject<BlockItem> PURPLE_DUNGEON_BRICKS = ITEMS.register("purple_dungeon_bricks", () -> new BlockItem(NeBlocks.PURPLE_DUNGEON_BRICKS.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
-    public static final RegistryObject<BlockItem> PURPLE_CRACKED_DUNGEON_BRICKS = ITEMS.register("purple_cracked_dungeon_bricks", () -> new BlockItem(NeBlocks.PURPLE_CRACKED_DUNGEON_BRICKS.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
-    public static final RegistryObject<BlockItem> PURPLE_MOSSY_DUNGEON_BRICKS = ITEMS.register("purple_mossy_dungeon_bricks", () -> new BlockItem(NeBlocks.PURPLE_MOSSY_DUNGEON_BRICKS.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
-    public static final RegistryObject<BlockItem> PURPLE_DUNGEON_PILLAR = ITEMS.register("purple_dungeon_pillar", () -> new BlockItem(NeBlocks.PURPLE_DUNGEON_PILLAR.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
-
     public static final RegistryObject<BlockItem> PURPLE_CHISELED_DUNGEON_BRICKS = ITEMS.register("purple_chiseled_dungeon_bricks", () -> new BlockItem(NeBlocks.PURPLE_CHISELED_DUNGEON_BRICKS.get(), new Item.Properties().tab(NeCreativeModeTab.NEKROMANT_BLOCKS)));
+    public static final RegistryObject<BlockItem> PURPLE_DUNGEON_BRICKS = ITEMS.register("purple_dungeon_bricks", () -> new BlockItem(NeBlocks.PURPLE_DUNGEON_BRICKS.get(), new Item.Properties().tab(NeCreativeModeTab.NEKROMANT_BLOCKS)));
+    public static final RegistryObject<BlockItem> PURPLE_CRACKED_DUNGEON_BRICKS = ITEMS.register("purple_cracked_dungeon_bricks", () -> new BlockItem(NeBlocks.PURPLE_CRACKED_DUNGEON_BRICKS.get(), new Item.Properties().tab(NeCreativeModeTab.NEKROMANT_BLOCKS)));
+    public static final RegistryObject<BlockItem> PURPLE_MOSSY_DUNGEON_BRICKS = ITEMS.register("purple_mossy_dungeon_bricks", () -> new BlockItem(NeBlocks.PURPLE_MOSSY_DUNGEON_BRICKS.get(), new Item.Properties().tab(NeCreativeModeTab.NEKROMANT_BLOCKS)));
+    public static final RegistryObject<BlockItem> PURPLE_DUNGEON_PILLAR = ITEMS.register("purple_dungeon_pillar", () -> new BlockItem(NeBlocks.PURPLE_DUNGEON_PILLAR.get(), new Item.Properties().tab(NeCreativeModeTab.NEKROMANT_BLOCKS)));
 
 }
 
