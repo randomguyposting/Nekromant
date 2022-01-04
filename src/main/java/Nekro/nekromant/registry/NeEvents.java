9d79ef1bd9949913c2238623d2dabeaf42a8f5ef
NouZe <36501230+randomguyposting@users.noreply.github.com>
2022-01-04 21:41:08 +0100

    made drop bloods only drop when zombies are hit (using an extractor)

--- src/main/java/Nekro/nekromant/registry/NeEvents.java
+++ src/main/java/Nekro/nekromant/registry/NeEvents.java
@@ -2,6 +2,7 @@ package Nekro.nekromant.registry;
 
 import Nekro.nekromant.Nekromant;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Items;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
@@ -16,8 +17,8 @@ public class NeEvents {
         if(!event.getEntity().level.isClientSide()){
             if(event.getSource().getDirectEntity() instanceof Player){
                 Player player = ((Player) event.getSource().getDirectEntity());
-                Entity entity = event.getEntityLiving();
-                if(player.getMainHandItem().getItem() == NeItems.EXTRACTER.get()){
+                EntityType entity = event.getEntity().getType();
+                if(player.getMainHandItem().getItem() == NeItems.EXTRACTER.get() && entity == EntityType.ZOMBIE){
                     event.getEntityLiving().spawnAtLocation(NeItems.BLOOD_DROP.get());
                 }
             }
